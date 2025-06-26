e23d3aab3d1bc27211b38ac90feccdd6c4d5b171
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-26 19:01:06 +0530

    Create krishnamurthy-num.c

---
+++ krishnamurthy-num.c
@@ -0,0 +1,29 @@
+/*krishnamurthy nummber(or)strong number*/
+#include<stdio.h>
+int main()
+{
+    int digit,fact,num=145,sum=0,temp=0;
+    printf("enter a number: ");
+    scanf("%d", &num);
+    temp=num;
+    while(temp!=0)
+    {
+        digit=temp%10;
+        fact=1;
+        for(int i=1;i<=digit;i++)
+        {
+        fact*=i;
+        }
+     sum+=fact;
+     temp/=10;
+    }
+    if(num==sum)
+    {
+        printf("%d is a strong", num);
+    }
+    else
+    {
+        printf("%d is not strong", num);
+    }
+    return 0;
+}
