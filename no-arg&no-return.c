17ce7d043d4fdfdaafcbe18cc8c38a358f04b92a
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-26 19:13:15 +0530

    Create no-arg&no-return.c

---
+++ no-arg&no-return.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int A()
+{
+	int x;
+	printf("Enter a number: ");
+	scanf("%d",&x);
+	return x;
+}
+int main()
+{
+	int num=A();
+	printf("Your value stored in function is %d",num);
+	return 0;
+}
