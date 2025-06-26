fc1762dba615086a318702d22c13d5c5137527d7
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-26 19:10:12 +0530

    Create func(no-arg&no-return).c

---
+++ func(no-arg&no-return).c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void Hello()
+{
+    printf("Hello all!!!");
+}
+int main()
+{
+    Hello();
+    return 0;
+}
+
+
+
+#include<stdio.h>
+void number()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    printf("You have entered %d",num);
+}
+int main()
+{
+    number();
+    return 0;
+}
