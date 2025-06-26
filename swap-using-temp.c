0a243706ceed82c8cddb26d2d0806cc634292148
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-26 18:49:29 +0530

    Create swap-using-temp.c

---
+++ swap-using-temp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,temp;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+    printf("Before swapping: %d,%d\n",a,b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("After swapping: %d,%d",a,b);
+	return 0;
+}
