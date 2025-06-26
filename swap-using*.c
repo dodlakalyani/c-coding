7fb375d1334bb54d8c4a7c295f7def904e4bcb1c
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-26 18:51:58 +0530

    Create swap-using*.c

---
+++ swap-using*.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a:");
+    scanf("%d",&a);
+    printf("Enter b:");
+    scanf("%d",&b);
+    printf("Before swapping: %d,%d\n",a,b);
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    printf("After swapping: %d,%d",a,b);
+	return 0;
+}
