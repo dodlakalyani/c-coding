e9cd4fb556fca461d6dffd3254059a3b9c67d13d
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-27 09:39:35 +0530

    Create ternary-op-macro.c

---
+++ ternary-op-macro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define max(a,b) ((a)>(b)?(a):(b))
+int main()
+{
+    int a,b;
+    printf("Enter a and b:");
+    scanf("%d %d",&a,&b);
+    int result=max(a,b);
+    printf("Large number is %d",result);
+    return 0;
+}
