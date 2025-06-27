b4d7144180a8f7a216d7291078664b74905f9caf
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-27 09:38:50 +0530

    Create obj-macro.c

---
+++ obj-macro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define LENGTH 5
+#define WIDTH 4
+int main()
+{
+    int area = LENGTH * WIDTH;
+    printf("Using macros for dimensions:\n");
+    printf("Length = %d, Width = %d\n", LENGTH, WIDTH);
+    printf("Area of rectangle is: %d\n", area);
+    return 0;
+}
