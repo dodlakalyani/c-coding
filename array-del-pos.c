b3a947cd4e3858c5d496411e4e2838def9a4d02c
dodlakalyani <dodlakalyani49@gmail.com>
2025-06-30 10:00:05 +0530

    Create array-del-pos.c

---
+++ array-del-pos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int arr[20],i,j,pos,n;
+    printf("Enter the no.of elements:");
+    scanf("%d",&n);
+    printf("Enter the Elements:\n");
+    for(i =0; i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the position of element to be deleted:");
+    scanf("%d",&pos);
+    for(i=pos;i<n;i++){
+        arr[i] = arr[i+1];
+    }
+    for(i =0; i<n-1;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
